b01ea306ee473ba412d20d99e18e73ae436c21b3
Niveytha Waran <45155417+Niveytha@users.noreply.github.com>
2026-03-26 23:16:09 +0800

    feat: add first component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
   return (
     <div>
       <h1>Smart Expense Tracker</h1>
       <p>My first React project is running locally 🚀</p>
+      <br />
+      <ExpenseItem />
     </div>
   );
 }
