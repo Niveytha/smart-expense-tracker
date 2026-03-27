539a13139720197f43e414a1d0546aae6109333d
Niveytha Waran <45155417+Niveytha@users.noreply.github.com>
2026-03-27 22:27:27 +0800

    feat: make ExpenseItem dynamic with props

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,9 @@ function App() {
       <h1>Smart Expense Tracker</h1>
       <p>My first React project is running locally 🚀</p>
       <br />
-      <ExpenseItem />
+      <ExpenseItem title="Food" amount={10.99} date="27 Mar 2026" />
+      <ExpenseItem title="Transport" amount={5.5} date="27 Mar 2026" />
+      <ExpenseItem title="Entertainment" amount={20.0} date="26 Mar 2026" />
     </div>
   );
 }
--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,5 +1,11 @@
-function ExpenseItem() {
-  return <div>Expense Item</div>;
+function ExpenseItem(props) {
+  return (
+    <div>
+      <h3>{props.title}</h3>
+      <p>${props.amount}</p>
+      <p>{props.date}</p>
+    </div>
+  );
 }
 
 export default ExpenseItem;
